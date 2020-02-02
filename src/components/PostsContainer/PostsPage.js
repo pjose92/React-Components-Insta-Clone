81a8b302fcd17f75d6e2aa9d12aa46f485b74db6
Jose <pjose14.jp38@yahoo.com>
2020-02-01 20:22:44 -0600

    number of likes are displaying

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,10 +4,18 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.data.map((p) => {
+        console.log('PropsPage', p)
+        return (
+          <div>
+            <Post post={p}/>
+          </div>
+        )
+      })}
     </div>
   );
 };
