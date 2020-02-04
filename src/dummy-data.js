37e82e95ff5f61b23ad8288574f4d14921f71c3c
Jose <pjose14.jp38@yahoo.com>
2020-02-03 20:16:55 -0600

    added some data to the dummy-data.js file

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,5 +1,55 @@
 const dummyData = [
     {
+        username: "AmIFake",
+        thumbnailUrl: 'https://simpleicon.com/wp-content/uploads/rocket.png',
+        imageUrl: 'https://images.unsplash.com/photo-1497935586351-b67a49e012bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80',
+        likes: 1807,
+        timestamp: "August 10th 2019, 03:12:09 pm",
+        comments: [
+            {
+                username: "thatoneguy",
+                text: "I love coffee!!!"
+            },
+            {
+                username: "IamFake",
+                text: "Wish I was drinking coffee too!"
+            },
+            {
+                username: "ImFakeToo",
+                text: "wow"
+            },
+            {
+                username: "everyoneisfake",
+                text: "that looks so good"
+            }
+        ]
+    },
+    {
+        username: "jpcodes_",
+        thumbnailUrl: 'https://icons-for-free.com/iconfiles/png/512/go+pikachu+play+pokemon+icon-1320186973527720987.png',
+        imageUrl: 'https://images.unsplash.com/photo-1580749908807-86f29695487b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        likes: 807,
+        timestamp: "August 10th 2019, 03:12:09 pm",
+        comments: [
+            {
+                username: "thatoneguy",
+                text: "cool pic!"
+            },
+            {
+                username: "IamFake",
+                text: "I love dogs!"
+            },
+            {
+                username: "ImFakeToo",
+                text: "amazing photo!"
+            },
+            {
+                username: "everyoneisfake",
+                text: "this is just all fake"
+            }
+        ]
+    },
+    {
         username: "philzcoffee",
         thumbnailUrl: 'https://instagram.fsjo9-1.fna.fbcdn.net/vp/d4728e507fe1eb2895fb7ca394bb6431/5E4122B1/t51.2885-15/s150x150/71118783_2434459423505735_6295784028176668008_n.jpg?_nc_ht=instagram.fsjo9-1.fna.fbcdn.net',
         imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
